4ab929d8fbe2b79d1fbdec513513e68eac1a4a90
Jerzy Drobot <jerzy.drobot@mggp.com.pl>
2017-09-25 11:29:52 +0200

    Component class

--- src/data/renderList.js
+++ src/data/renderList.js
@@ -1,24 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-const renderList = (events)=> {
-    return (
-        <ul>
-            {events.map((x) => {
-                let date = Date.parse(x.date);
-                if (date >= Date.now()) {
-                    return (
-                        <li key={x.id}>
-                            <strong>{x.name}</strong><br />
-                            Gdzie: {x.place} <br />
-                            Kiedy: {x.date} {x.time}
-                        </li>
-                    )
-                };
-                return null;
-            })
-            }
-        </ul>
-    );
+//Funkcja pobierająca wydarzenia
+//props jako parametr stusujemy przy zamianie fukncji w komponent
+class Meetings extends React.Component {
+    static propTypes = {
+       meetings: PropTypes.array.isRequired
+    };
+    render() {
+        return (
+            <ul>
+                {this.props.meetings.map((x) => {
+                    //  let date = new Date(x.date) - inny sposób na konwersję daty ze stringa
+                    let date = Date.parse(x.date);
+                    if (date >= Date.now()) {
+                        return (
+                            <li key={x.id}>
+                                <strong>{x.name}</strong><br />
+                                Gdzie: {x.place} <br />
+                                Kiedy: {x.date} {x.time}
+                            </li>
+                        )
+                    };
+                    return null;
+                })
+                }
+            </ul> 
+        );
+    }
 }
 
-export default renderList  ;
+// Drugi sposób rozwiązania zadania: Wyświetl spotkania z dat przyszłych  
+            // <ul>
+            //     {meetings.filter(y => Date.parse(y.date) >= Date.now()).map((x) => {
+            //         return <li key={x.id}>
+            //             <strong>{x.name}</strong><br />
+            //             Gdzie: {x.place} <br />
+            //             Kiedy: {x.date} {x.time}</li>;
+            //     })
+            //     }
+            // </ul>
+
+export { Meetings };
--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import events from './data/events.json'
-import renderList from './data/renderList.js'
+import meetings from './data/events.json'
+import { Meetings } from './data/renderList.js'
 
 ReactDOM.render(
-    renderList(events)
-    // <ul>
-    //     {events.filter(y => Date.parse(y.date) >= Date.now()).map((x) => {
-    //         return <li key={x.id}>
-    //             <strong>{x.name}</strong><br />
-    //             Gdzie: {x.place} <br />
-    //             Kiedy: {x.date} {x.time}</li>;
-    //     })
-    //     }
-    // </ul>
-
-    , document.getElementById('root'));
+    <Meetings meetings={meetings} /> , document.getElementById('root'));
