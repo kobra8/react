717594fac5647a5ad4bd8e848ffc5afa3dff016f
Jerzy Drobot <jerzy.drobot@mggp.com.pl>
2017-09-29 16:21:33 +0200

    Cykle komponentu

--- src/data/renderList.js
+++ src/data/renderList.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import meetings from './events.json'
 import { CounterComponent } from './counter';
 
 //Funkcja pobierająca wydarzenia
 //props jako parametr stusujemy przy zamianie fukncji w komponent
 class Meetings extends React.Component {
-    static propTypes = {
-        meetings: PropTypes.array.isRequired
-    };
+    //Kontrola typu, jeżeli dane będą przkazywane w innym komponencie: 
+    // static propTypes = {
+    //     meetings: PropTypes.array.isRequired
+    // };
+    constructor(props) {
+        super(props);
+        this.state = {
+            meetings: []
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            meetings 
+            //Powyżej uzyto ES6, która skraca zapis meetings: meetings
+        })
+    }
+
     render() {
         return (
             <div>
                 <h2>Lista spotkań:</h2>
                 <ul>
-                    {this.props.meetings.map((x) => {
+                    {this.state.meetings.map((x) => {
                         //  let date = new Date(x.date) - inny sposób na konwersję daty ze stringa
                         let date = Date.parse(x.date);
                         if (date >= Date.now()) {
@@ -29,7 +45,7 @@ class Meetings extends React.Component {
                     })
                     }
                 </ul>
-                    <CounterComponent />
+                <CounterComponent />
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import meetings from './data/events.json'
 import { Meetings } from './data/renderList.js'
 
 ReactDOM.render(
-    <Meetings meetings={meetings} /> , document.getElementById('root'));
+    <Meetings />, document.getElementById('root'));
